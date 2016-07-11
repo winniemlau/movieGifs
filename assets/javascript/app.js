792570ba876700c4d326d40730a499866553813f
winniemlau <wmlau26@gmail.com>
2016-07-11 18:33:07 -0400

    styling changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	});
 });
 
-function appendNewButton(movie){ 
+function appendNewButton(movie){
 	    var a = $('<button>')
 	    a.addClass('movie');
 	    a.attr('data-name', movie);
@@ -23,7 +23,7 @@ function appendNewButton(movie){
       a.on('click', displayMovieInfo);
 	    $('#buttonsView').append(a);
 	}
-	function renderButtons(){ 
+	function renderButtons(){
 		for (var i = 0; i < movies.length; i++){
 		    appendNewButton(movies[i])
 		}
@@ -32,36 +32,37 @@ function appendNewButton(movie){
 		var movie = $(this).data('name');
 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10";
 		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-      
+
       var results = response.data;
       console.log(results);
-      
+
         	for (var i = 0; i < results.length; i++) {
 				 displayGif(results[i]);
-           
-			} 
-        
+
+			}
+
 		});
     clearMyDiv();
 	}
 function displayGif(results){
 
 		var movieDiv = $('<div>');
+				movieDiv.addClass('imagestyling');
         var p = $('<p>').text("Rating: " + results.rating);
         var movieImage = $('<img>');
         movieImage.attr('src', results.images.fixed_height_still.url);
-        movieImage.attr('data-still', results.images.fixed_height_still.url);              
-        movieImage.attr('data-animate', results.images.fixed_height.url);              
-        movieImage.attr('data-state', "still");  
-        movieImage.attr('class', "movieImage");    
-        movieImage.on('click', toggleImg);           
+        movieImage.attr('data-still', results.images.fixed_height_still.url);
+        movieImage.attr('data-animate', results.images.fixed_height.url);
+        movieImage.attr('data-state', "still");
+        movieImage.attr('class', "movieImage");
+        movieImage.on('click', toggleImg);
         movieDiv.append(p);
         movieDiv.append(movieImage);
         $('#gifsAppearHere').prepend(movieDiv);
 	}
-  
+
 function toggleImg(){
-		var state = $(this).attr('data-state'); 
+		var state = $(this).attr('data-state');
 	 		if ( state == 'still'){
                $(this).attr('src', $(this).data('animate'));
                $(this).attr('data-state', 'animate');
@@ -75,4 +76,3 @@ function clearMyDiv(){
   $('#gifsAppearHere').empty();
   $('#giftsAppearHere').html('');
 }
-
